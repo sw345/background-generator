9733a191fe78183b10e2c46e6beffe9b562ade39
Samson Wong <samson@Office-iMac.local>
2019-05-02 21:03:03 -0400

    adding starting project

---
+++ app.js
@@ -0,0 +1,6 @@
+
+document.querySelector('#test-button').addEventListener('click',function()
+{
+  console.log('hi')
+    document.querySelector('#sentence').innerText = `Post-infiltrate care included .`  
+})
